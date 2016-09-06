4818f4354fdf11690d3a2bff7b85685272dac78e
jenitha baskaran <jenijerish21@gmail.com>
2016-09-06 13:00:38 +0530

    Create unique.java

---
+++ unique.java
@@ -0,0 +1,31 @@
+public class Array {
+
+	public static void main(String[] args) 
+	{
+		int m = args.length;
+		int []a=new int[m];
+		int []b=new int[m];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=Integer.parseInt(args[i]);		
+		b[i]=0;
+		}
+		for(int i=0;i<n;i++)
+		{
+		for(int j=0;j<n;j++)
+		{
+			if(a[i]==a[j])
+			{
+				b[i]=b[i]+1;
+			}
+			
+			}}
+		for(int i=0;i<n;i++)
+		{
+			if(b[i]<2)
+			{
+			System.out.println("The unique element is " +a[i]);
+			}
+			}
+		}
+	}
