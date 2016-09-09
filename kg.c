1e7a823a6945327e717f697fbfa4502029b02ad8
jenitha baskaran <jenijerish21@gmail.com>
2016-09-09 12:34:57 +0530

    Update kg.c

--- kg.c
+++ kg.c
@@ -2,16 +2,16 @@
 #include<conio.h>
 void main()
 {
-int n1,x;
+int y,x;
 int p,i,j=1;
 clrscr();
-printf("Enter the n value : ");
-scanf("%d",&n1);
+printf("Enter the y value : ");
+scanf("%d",&y);
 printf("Enter the pair : ");
 scanf("%d",&p);
-for(i=0;i<2*n1;i++)
+for(i=0;i<2*y;i++)
 {
-x=2*n1-i;
+x=2*y-i;
 if(p!=x)
 {
 printf("The pair for %d  day  is %d and %d\n",p,j++,p);
