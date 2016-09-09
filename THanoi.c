9e9296816802ed02b788c899cf1e391d5da888cd
jenitha baskaran <jenijerish21@gmail.com>
2016-09-09 16:23:39 +0530

    Create THanoi.c

---
+++ THanoi.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+void hanoi1(char,char,char,int);
+void main()
+{
+int z;
+clrscr();
+printf(" Enter the num of disk: ");
+scanf("%d",&z);
+printf("\n tower of honai %d number of disk : ", z);
+hanoi1('A','B','C', z);
+getch();
+}
+void hanoi1(char from,char to,char other,int b)
+{
+if(b<=0)
+printf("\n Enter a valid disk");
+if(b==1)
+printf("\n Move disk from %c to %c",from,other);
+if(b>1)
+{
+hanoi1(from,other,to,b-1);
+hanoi1(from,to,other,1);
+hanoi1(to,from,other,b-1);
+}
+  }
