b4e5cbde39fc2051568b3de5ac3cd9067d796804
jenitha baskaran <jenijerish21@gmail.com>
2016-09-16 09:43:32 +0530

    Create fivehello.java

---
+++ fivehello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class fivehello
+{
+ public static void main(String args[])
+ {
+  int i;
+  for(i=0;i<5;i++)
+  {
+   System.ou.println("Hello World");
+  }
+  
+  }
+}
