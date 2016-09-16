c4b7e736f34f39cf321f8dc9a432f6aa408bc9b0
jenitha baskaran <jenijerish21@gmail.com>
2016-09-16 08:25:10 +0530

    Create charalpha.java

---
+++ charalpha.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class charalpha
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  String s=sc.next();
+  char[] ch=s.toCharArray();
+  for(int i=0;i<s.length;i++)
+  {
+   if(ch[i]>='a' && ch[i]<='z' || ch[i]>='A' && ch[i]<='Z')
+   {
+    System.out.println(ch[i]+" is character");
+    }
+    else
+    {
+    System.out.println("not a character");
+  }
+}
+   
